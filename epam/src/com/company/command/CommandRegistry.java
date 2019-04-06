1bf1b42d07a4f77283aac4915d2575298ba185d8
Evg Red <voiceofdust@gmail.com>
2019-04-06 23:48:53 +0300

    New commit
    Ne showCommands

--- epam/src/com/company/command/CommandRegistry.java
+++ epam/src/com/company/command/CommandRegistry.java
@@ -3,8 +3,16 @@ package com.company.command;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * Makes registry of commands and add it in HashMap to use it in tha application.
+ */
 public enum CommandRegistry {
     INSTANCE;
+
+    /**
+     * Makes HashMap to save all available commands.
+     */
       static   Map<String,ACommand> commands;
 
     static {
@@ -14,8 +22,21 @@ public enum CommandRegistry {
         commands.put("name", new CommandName("name"));
     }
 
+    /**
+     * @return Object type ACommand
+     * @param name require a name of a command to return.
+     */
     public ACommand getCommand (String name){
         return commands.get(name);
+    }
+
+    /**
+     * Method to write in console available commands.
+     */
+    public void showCommands (){
+        for (Map.Entry<String, ACommand> entry : commands.entrySet()){
+            System.out.println(entry.getKey());
+        }
     }
 
 }
