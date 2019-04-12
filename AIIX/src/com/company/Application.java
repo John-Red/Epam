759a43eb23e49365ea35421aae7508112bb4dd03
John Red <voiceofdust@gmail.com>
2019-04-12 12:33:10 +0300

    Refactor

--- AIIX/src/com/company/Application.java
+++ AIIX/src/com/company/Application.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.State.ApplicationState;
 import  com.company.command.*;
 
 
@@ -9,10 +10,13 @@ public class Application {
     static public final String AUTHOR = "Evgeniy Redin";
     static public final String VERSION = "0.0.0";
 
+
     public static void main(String[] args) {
         CommandRegistry.INSTANCE.showCommands();
         String currentName = "version";
         ACommand command = CommandRegistry.INSTANCE.getCommand(currentName);
         command.execute();
+
+
     }
 }
---
+++ AIIX/src/com/company/command/CommandWeather.java
@@ -0,0 +1,15 @@
+package com.company.command;
+
+
+public class CommandWeather extends ACommand{
+
+    CommandWeather(String name) {
+        super(name);
+    }
+
+    @Override
+    public void execute() {
+
+        System.out.println();
+    }
+}
